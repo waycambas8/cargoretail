f4c410020065fcda472eca4dcfc8be1a96afba15
waycambas9 <waycambas9@gmail.com>
2022-01-26 15:23:16 +0700

    Update Beta 1.0.2

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+ <footer class="main-footer">
+  <strong>Copyright &copy; 2021-2025 <a href="https://adminlte.io">PT DIRA PRATAMA EXPRESSINDO</a>.</strong>
+  All rights reserved.
+  <div class="float-right d-none d-sm-inline-block">
+    <b>Version</b> Beta 0.0.1
+  </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware'=>'login'],function(){
+    Route::get("/",[DashboardController::class,"dashboard"])->name("dashboard");
 });
+
+Route::get("login",function(){
+    return view("modul.login.login");
+})->name("login");
